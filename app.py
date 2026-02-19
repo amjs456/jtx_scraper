130c8e5c542ce66933d8ea3add50d471ea4adaaf
yuki <uk.tmbc2408@gmail.com>
2026-02-20 08:19:19 +0900

    ok

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ JTX_URL = "https://www.jointex.co.jp/DFiDtl001Servlet?orderCode="
 order_codes=[
     "832151",
     "349813",
-    "483901"
+    "483901",
+    "30600",
+    "32351"
 ]
 
 items=[]
@@ -18,15 +20,13 @@ def run(Playwright, order_codes):
     item_n = len(order_codes)
     for i, order_code in enumerate(order_codes):
         page.goto(JTX_URL+order_code)
-        code = None
-        maker = None
-        url = None
-        name = None
-        price = None
-        is_price_red = None
+        code = maker = url = name = price = is_price_red = None
+        is_not_six_digit = False
+
+        if not len(order_code)==6:
+            is_not_six_digit = True
 
         if not page.title()=="エラー画面":
-        
             price = page.locator("div.rBox aside p.txt02").nth(1).inner_html().replace(" ", "").replace("\n", "")[:-1]
             if price.startswith("<font"):
                 price=price[21:-7]
@@ -36,7 +36,7 @@ def run(Playwright, order_codes):
             url = page.url
             name = page.locator("div.info h2").inner_html()
 
-        item_info = [maker, name, price, is_price_red, code, url]
+        item_info = [maker, name, price, is_price_red, code, url, is_not_six_digit]
         items.append(item_info)
         print(str(i+1)+"/"+str(item_n)+" completed!")
     browser.close()
